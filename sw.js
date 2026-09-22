c78551143e2bb2498a12ca8343b453f556acd114
haomanioush1404-pixel <haomanioush1404@gmail.com>
2026-09-22 22:53:03 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hesabdari-v1';
+const CACHE_NAME = 'hesabdari-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
